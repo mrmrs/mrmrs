157e8f69b711a1245bc2d0e784b99652ec3e7408
mrmrs <hi@mrmrs.cc>
2019-06-04 00:08:54 -0400

    Add new site?

---
+++ precache-manifest.88f4c32009eaaaa988c6153897e6c946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da43bf75e6d1418e766ed8037a7aaeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bdd1804e20465d0609",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "c698e38d2943089ad4ac",
+    "url": "/static/js/2.43b49671.chunk.js"
+  },
+  {
+    "revision": "f2bdd1804e20465d0609",
+    "url": "/static/js/main.461a1f9d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
